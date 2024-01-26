ef7e674834c81577705d548b33b4ac594b3df621
Crowdbotics <team@crowdbotics.com>
2024-01-26 18:08:48 +0000

    Initial commit

---
+++ backend/hut_hut_full_featur_45576/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for hut_hut_full_featur_45576 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "hut_hut_full_featur_45576.settings"
+)
+
+application = get_wsgi_application()
